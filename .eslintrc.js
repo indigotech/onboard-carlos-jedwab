861f0af0c4d32eef8f6eba4e614afc0a10436084
carlosjedwab <taqtile@Vedett.local>
2022-01-04 18:51:00 -0300

    Fix configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,21 @@
 module.exports = {
-  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
 };
