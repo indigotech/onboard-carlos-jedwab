e1cab0bb64f559d4898d00c5210493d4fadd0947
carlosjedwab <carlosjedwab@hotmail.com>
2022-01-06 18:40:51 -0300

    Add the spinning loading indicator

---
+++ src/components/button-input.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+
+import { Text } from './text';
+
+interface ButtonInputProps {
+  label: string;
+  type?: 'button' | 'submit' | 'reset';
+  isLoading?: boolean;
+  onClick?: () => void;
+}
+
+export const ButtonInput = (props: ButtonInputProps) => {
+  return (
+    <div className='ButtonInput'>
+      <button className='ButtonInput__button' type={props.type} onClick={props.onClick}>
+        {props.isLoading && (
+          <img
+            className='ButtonInput__loading'
+            alt='loading-indicator'
+            src='http://cdn.onlinewebfonts.com/svg/img_1585.png'
+          />
+        )}
+        <Text type='button'>{props.label}</Text>
+      </button>
+    </div>
+  );
+};
