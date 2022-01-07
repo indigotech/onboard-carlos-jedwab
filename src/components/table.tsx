3c8f73d4e2650d73474eab9e5858f7b99ab23202
carlosjedwab <carlosjedwab@hotmail.com>
2022-01-07 17:22:29 -0300

    Apply sugestion

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -3,33 +3,30 @@ import './style.css';
 
 import { Text } from './text';
 
-export type RowType = {
-  [key: string]: string;
-};
+export type HeaderType = string[];
+export type RowType = Record<string, string>;
 
 interface TableProps {
-  header: RowType;
+  header: HeaderType;
   rows: RowType[];
-  renderHeaderItem?: (key: string, value: string) => JSX.Element;
-  renderRowItem?: (key: string, item: string) => JSX.Element;
 }
 
 export const Table = (props: TableProps) => {
   return (
     <div className='Table'>
       <div className='Table__header'>
-        {Object.entries(props.header).map(([key, item]) => (
-          <div className='Table__item' key={key}>
-            {props.renderHeaderItem ? props.renderHeaderItem(key, item) : <Text type={'header'}>{item}</Text>}
+        {props.header.map((item, i) => (
+          <div className='Table__item' key={i}>
+            {<Text type={'header'}>{item}</Text>}
           </div>
         ))}
       </div>
 
       {props.rows.map((row, i) => (
         <div className='Table__row' key={i}>
-          {Object.entries(row).map(([key, item], j) => (
-            <div className='Table__item' key={j}>
-              {props.renderRowItem ? props.renderRowItem(key, item) : <Text type={'label'}>{item}</Text>}
+          {Object.entries(row).map(([key, item]) => (
+            <div className='Table__item' key={key}>
+              {<Text type={'label'}>{item}</Text>}
             </div>
           ))}
         </div>
--- src/pages/front-page.tsx
+++ src/pages/front-page.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
 import { Spinner } from '../components/spinner';
-import { Table, RowType } from '../components/table';
+import { Table, HeaderType, RowType } from '../components/table';
 
 export const FrontPage = () => {
   // TODO: Change isLoading to a state and show it only when during this page's request
   const isLoading = true;
 
   // TODO: Use the data from a request
-  const header: RowType = {
-    col1: 'Nome',
-    col2: 'Email',
-  };
+  const header: HeaderType = ['Nome', 'Email'];
 
   const rows: RowType[] = [
     {
