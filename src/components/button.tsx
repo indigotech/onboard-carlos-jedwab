af951f46187c93a6cc7d8e18d35b39f9e6ca9aac
carlosjedwab <carlosjedwab@hotmail.com>
2022-01-07 11:16:39 -0300

    Rename ButtonInput to Button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -15,7 +15,7 @@ export const Button = (props: ButtonProps) => {
   return (
     <div className='Button'>
       <button className='Button__button' type={props.type} disabled={props.isLoading} onClick={props.onClick}>
-        {props.isLoading && <Spinner size='small' />}
+        <Spinner hide={!props.isLoading} size='small' />
         <div className='Button__label'>
           <Text type='button'>{props.label}</Text>
         </div>
