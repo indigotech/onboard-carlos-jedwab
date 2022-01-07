1d073fa1cb9df7b37c351ba47662f727f49af934
carlosjedwab <carlosjedwab@hotmail.com>
2022-01-07 10:16:07 -0300

    Add the spinner to the front page

--- src/pages/front-page.tsx
+++ src/pages/front-page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Spinner } from '../components/spinner';
+
 export const FrontPage = () => {
+  // TODO: Change isLoading to a state and show it only when during this page's request
+  const isLoading = true;
+
   return (
     <div>
       <h1>FrontPage</h1>
+      <Spinner hide={!isLoading} size='medium' />
     </div>
   );
 };
